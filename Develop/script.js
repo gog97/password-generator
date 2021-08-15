b9a0952f5df963216578bbcfcf70255da978c018
gzflykid <ozgarcia97@gmail.com>
2021-08-15 15:21:17 -0600

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -2,27 +2,44 @@
  function generatePassword(){
    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+   var numbers = "0123456789"
    var allOptions = "";
    var finalPassword = "";
    var userPasswordLength = prompt("Enter your password length between 8-128");
    var userLowerCase = confirm("Do you want to use lowercase?");
    var userUppercase = confirm("Do you want to use uppercase?");
+   var numbers = confirm("Do you want numbers?")
+   var index = 0
+
+   console.log(allOptions)
 
    if (userUppercase == true){
      allOptions = allOptions + uppercase;
    }
 
+   console.log(allOptions)
+
    if (userLowerCase == true){
      allOptions = allOptions + lowerCase;
    }  
 
-   for (let i = 0; i < allOptions.length; i++) {
-     
+   console.log(allOptions)
+
+   if (numbers == true){
+     allOptions = allOptions + numbers;
+   }
+
+   console.log(allOptions)
+
+   for (let i = 0; i < userPasswordLength; i++) {
+     index = Math.floor(Math.random() * allOptions.length)
+     finalPassword = finalPassword + allOptions[index]
    } 
 
 
    return finalPassword;
  }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
